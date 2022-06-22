268eb331e46df86dd2f3d112cc5960bad1c96cbe
karthika777 <karthikanagarajan100@gmail.com>
2022-06-22 17:11:55 +0530

    first commit

---
+++ vpc.tf
@@ -0,0 +1,110 @@
+/* ================== Provider ====================== */
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA5JKCP6HX5LPPXA6P"
+  secret_key = "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"
+}
+
+/* ==================== VPC ============================= */
+
+# Create a VPC
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Terra-VPC"
+  }
+}
+
+# Create Web Public Subnet
+resource "aws_subnet" "web-subnet-1" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Websubterra-1a"
+  }
+}
+
+resource "aws_subnet" "web-subnet-2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Websubterra-2b"
+  }
+}
+
+# Create Application Private Subnet
+resource "aws_subnet" "application-subnet-1" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Applicationsubterra-1a"
+  }
+}
+
+resource "aws_subnet" "application-subnet-2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "Applicationsubterra-2b"
+  }
+}
+
+/* ======================== IGW & RT =========================== */
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = "Terra-IGW"
+  }
+}
+
+# Create Web layber route table
+resource "aws_route_table" "web-rt" {
+  vpc_id = aws_vpc.my-vpc.id
+
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Webterra-RT"
+  }
+}
+
+# Create Web Subnet association with Web route table
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.web-subnet-1.id
+  route_table_id = aws_route_table.web-rt.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.web-subnet-2.id
+  route_table_id = aws_route_table.web-rt.id
+}
